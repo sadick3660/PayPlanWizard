f4c6ec3b77628aaa2b72592bfab3da996af28df3
MrDino-Tz <mrdinotz@email.com>
2025-11-11 17:22:51 +0300

    a viteconfig

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+
+
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  base: mode === 'production' ? '/PayPlanWizard/' : '/', // ← Production only
 }));
